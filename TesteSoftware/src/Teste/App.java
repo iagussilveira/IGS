a88b3ed4684cc49e37a3fa31108b0766f11d9de3
teste <teste@10.32.175.84>
2017-09-12 14:41:46 -0300

    Primeira versão

---
+++ TesteSoftware/src/Teste/App.java
@@ -0,0 +1,18 @@
+package Teste;
+
+public class App 
+{
+
+	public static void main( String[] args )
+	{
+	
+	double a = 10;
+	double b = 20;
+	double r = Sum.sum(a, b);
+	System.out.printf("A= %f\n", a);
+	System.out.printf("B= %f\n", b);
+	System.out.printf("SUM= %f\n", r);
+	
+	
+	}
+}
---
+++ TesteSoftware/src/Teste/Sum.java
@@ -0,0 +1,10 @@
+package Teste;
+
+public class Sum {
+
+	public static double sum(double a, double b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
